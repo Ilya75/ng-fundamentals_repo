c966f5d581c40d49c628432095f5a1ef1073a915
Ilya Ovchelupov <iovchelupoff@live.com>
2020-08-15 01:14:20 -0400

    Added routing to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent} from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetails } from '../app/events/event-details/event-details.component';
+import { EventDetailsComponent } from '../app/events/event-details/event-details.component';
+import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/error-404.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+import { appRoutes } from '../app/routes';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
-    EventDetails
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
   ],
-  providers: [ EventService, ToastrService ],
+  providers: [ EventService, ToastrService, EventRouteActivator ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
